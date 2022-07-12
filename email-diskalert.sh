72e58c759f7b5483fd81205fd9923edeeb4b8f65
NRockBR <40874291+nrockbr@users.noreply.github.com>
2022-07-11 21:57:19 -0300

    Update email-diskalert.sh

--- email-diskalert.sh
+++ email-diskalert.sh
@@ -1,10 +1,13 @@
-#!/bin/bash  
+#!/bin/bash
 
-CURRENT=$(df / | grep / | awk '{ print $5}' | sed 's/%//g')  
-THRESHOLD=90  
-   
-if [ "$CURRENT" -gt "$THRESHOLD" ] ; then  
-mail -s 'Disk Space Alert' myemail@domain.com << EOF  
-Your root partition remaining free space is critically low. Used: $CURRENT%  
-EOF  
+# VARIABLES
+CURRENT=$(df / | grep / | awk '{ print $5}' | sed 's/%//g')
+THRESHOLD=90
+
+
+# EXECUTION
+if [ "$CURRENT" -gt "$THRESHOLD" ] ; then
+mail -s 'Disk Space Alert' myemail@domain.com << EOF
+Your root partition remaining free space is critically low. Used: $CURRENT%
+EOF
 fi
