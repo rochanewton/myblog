af9d4a36a554168f1a3159c1bab1947a2acd9769
NRockBR <40874291+nrockbr@users.noreply.github.com>
2022-07-11 22:28:15 -0300

    Add hostname

--- email-diskalert.sh
+++ email-diskalert.sh
@@ -8,11 +8,14 @@
 CURRENT=$(df / | grep / | awk '{ print $5}' | sed 's/%//g')
 THRESHOLD=90
 EMAIL=myemail@domain.com
-
+HOSTNAME=$"(hostname -f)"
+#
 # --------------------- EXECUÇÃO ----------------------------------------- #
 if [ "$CURRENT" -gt "$THRESHOLD" ] ; then
 mail -s 'Disk Space Alert' $EMAIL << EOF
-Your root partition remaining free space is critically low. Used: $CURRENT%
+Your root partition remaining free space is critically low.
+Server: $HOSTNAME
+Used: $CURRENT%
 EOF
 fi
 # ------------------------------------------------------------------------ #
